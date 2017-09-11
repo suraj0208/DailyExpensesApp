fd5be1b7ee94be1ee0cc844055631ce8d757ce8a
suraj0208 <surajkumargunjal@gmail.com>
2017-09-11 22:59:29 +0530

    edit item with tag

--- app/src/main/java/com/suraj/dailyexpenses/EditItemActivity.java
+++ app/src/main/java/com/suraj/dailyexpenses/EditItemActivity.java
@@ -4,13 +4,19 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.suraj.dailyexpenses.data.BasicItem;
+
+import java.util.List;
 
 public class EditItemActivity extends AppCompatActivity {
     private BasicItem basicItem;
@@ -18,7 +24,7 @@ public class EditItemActivity extends AppCompatActivity {
     private EditText etEditItemReason;
     private EditText etEditItemAmount;
 
-    private CheckBox checkboxEditInfrequent;
+    private EditText etEditTag;
 
 
     @Override
@@ -36,7 +42,7 @@ public class EditItemActivity extends AppCompatActivity {
 
         (etEditItemReason = (EditText) findViewById(R.id.etEditItemReason)).setText(basicItem.getReason());
         (etEditItemAmount = (EditText) findViewById(R.id.etEditItemAmount)).setText(Integer.toString(basicItem.getAmount()));
-        //(checkboxEditInfrequent = (CheckBox) findViewById(R.id.checkboxEditInfrequent)).setChecked(basicItem.isInFrequent());
+        (etEditTag = (EditText) findViewById(R.id.etEditTag)).setText(basicItem.getTag());
 
         (findViewById(R.id.btnEditDelete)).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,6 +62,69 @@ public class EditItemActivity extends AppCompatActivity {
                         .show();
             }
         });
+
+        class BooleanHolder {
+            boolean bool;
+        }
+
+        final BooleanHolder prevFocus = new BooleanHolder();
+
+        etEditTag.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+
+                if (!prevFocus.bool) {
+                    showTagsDialog();
+                }
+
+                if (b) {
+                    prevFocus.bool = true;
+                } else {
+                    prevFocus.bool = false;
+                }
+
+            }
+        });
+
+        etEditTag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTagsDialog();
+            }
+        });
+    }
+
+    public void showTagsDialog() {
+        LayoutInflater layoutInflater = LayoutInflater.from(EditItemActivity.this);
+
+        View dialogView = layoutInflater.inflate(R.layout.dialog_tags, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(EditItemActivity.this);
+
+        ListView listViewTags = (ListView) dialogView.findViewById(R.id.listViewTags);
+
+        dialogView.findViewById(R.id.btnDone).setVisibility(View.GONE);
+
+        final List<String> tags = Utils.getAllTags();
+
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<>(EditItemActivity.this, android.R.layout.simple_list_item_1, tags);
+
+        builder.setView(dialogView);
+
+        listViewTags.setAdapter(itemsAdapter);
+
+
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        listViewTags.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                etEditTag.setText(tags.get(i));
+                alertDialog.dismiss();
+            }
+        });
     }
 
     @Override
@@ -70,7 +139,7 @@ public class EditItemActivity extends AppCompatActivity {
 
         switch (id) {
             case R.id.action_edit_save:
-                if (etEditItemAmount.getText().length() == 0 || etEditItemReason.getText().length() == 0) {
+                if (etEditItemAmount.getText().length() == 0 || etEditItemReason.getText().length() == 0 || etEditTag.getText().length() == 0) {
                     Utils.showToast(R.string.validDataError);
                     break;
                 }
@@ -80,7 +149,7 @@ public class EditItemActivity extends AppCompatActivity {
 
                 newBasicItem.setReason(etEditItemReason.getText().toString());
                 newBasicItem.setAmount(Integer.parseInt(etEditItemAmount.getText().toString()));
-                //newBasicItem.setInFrequent(checkboxEditInfrequent.isChecked());
+                newBasicItem.setTag(etEditTag.getText().toString());
 
                 Utils.editItem(newBasicItem);
                 Utils.showToast(R.string.editSuccess);
